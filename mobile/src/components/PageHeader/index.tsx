b1d5c0f0d2226d2640d04fb19f1c2ea3352e295c
lmaoclost <renansmoliveira@gmail.com>
2020-08-09 13:43:32 -0300

    Finish Mobile App

--- mobile/src/components/PageHeader/index.tsx
+++ mobile/src/components/PageHeader/index.tsx
@@ -1,37 +1,43 @@
-import React from 'react';
-import { View, Text, Image } from 'react-native';
+import React, { ReactNode } from 'react';
+import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
-import backIcon from '../../assets/images/icons/back.png';
-import logoImg from '../../assets/images/logo.png';
+import backIcon from '../../assets/images/icons/back.png'
+import logoImg from '../../assets/images/logo.png'
 
 import styles from './styles';
 
 interface PageHeaderProps {
-  title: string;
+    title: string;
+    headerRight?: ReactNode;
 }
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }: PageHeaderProps) => {
-  const { navigate } = useNavigation();
+const PageHeader: React.FunctionComponent<PageHeaderProps> = ({ title, headerRight, children }) => {
+    const { navigate } = useNavigation();
 
-  function handleGoBack() {
-    navigate('Landing');
-  }
+    function handleGoBack() {
+        navigate('Landing');
+    }
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.topBar}>
-        <BorderlessButton onPress={handleGoBack}>
-          <Image source={backIcon} resizeMode="contain" />
-        </BorderlessButton>
+    return (
+        <View style={styles.container}>
+            <View style={styles.topBar}>
+                <BorderlessButton onPress={handleGoBack}>
+                    <Image source={backIcon} resizeMode="contain" />
+                </BorderlessButton>
 
-        <Image source={logoImg} resizeMode="contain" />
-      </View>
+                <Image source={logoImg} resizeMode="contain" />
+            </View>
 
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
+            <View style={styles.header}>
+                <Text style={styles.title}>{title}</Text>
+                {headerRight}
+            </View>
+
+            {children}
+        </View>
+    )
 }
 
 export default PageHeader;
--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
 
@@ -7,53 +7,65 @@ import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons/study.png';
-import giveClassIcon from '../../assets/images/icons/give-classes.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
+import api from '../../services/api';
 
 function Landing() {
-  const { navigate } = useNavigation();
+    const { navigate } = useNavigation();
 
-  function handleNavigateToGiveClassesPage() {
-    navigate('GiveClasses');
-  }
+    const [totalConnections, setTotalConnections] = useState(0);
 
-  function handleNavigateToStudyPages() {
-    navigate('Study');
-  }
+    useEffect(() => {
+        api.get('connections').then(response => {
+            const { total } = response.data;
 
-  return (
-    <View style={styles.container}>
-      <Image source={landingImg} style={styles.banner} />
+            setTotalConnections(total);
+        })
+    }, []);
 
-      <Text style={styles.title}>
-        Seja bem-vindo, {'\n'}
+    function hadleNavigateToGiveClassesPage(){
+        navigate('GiveClasses');
+    }
+
+    function handleNavigateToStudyPage(){
+        navigate('Study');
+    }
+
+    return (
+        <View style={styles.container}>
+            <Image source={landingImg} style={styles.banner}/>
+
+            <Text style={styles.title}>
+            Seja bem-vindo, {'\n'}
         <Text style={styles.titleBold}>O que deseja fazer?</Text>
-      </Text>
+            </Text>
+            <View style={styles.buttonsContainer}>
+                <RectButton 
+                    onPress={handleNavigateToStudyPage}
+                    style={[styles.button, styles.buttonPrimary]}
+                >
+                    <Image source={studyIcon}/>
+                    <Text style={styles.buttonText}>Estudar</Text>
+                </RectButton>
 
-      <View style={styles.buttonsContainer}>
-        <RectButton
-          onPress={handleNavigateToStudyPages}
-          style={[styles.button, styles.buttonPrimary]}
-        >
-          <Image source={studyIcon} />
-          <Text style={styles.buttonText}>Estudar</Text>
-        </RectButton>
+                <RectButton 
+                    onPress={hadleNavigateToGiveClassesPage} 
+                    style={[styles.button, styles.buttonSecondary]}
+                >
+                    <Image source={giveClassesIcon}/>
+                    <Text style={styles.buttonText}>Dar Aulas</Text>
+                </RectButton>
+            </View>
 
-        <RectButton
-          onPress={handleNavigateToGiveClassesPage}
-          style={[styles.button, styles.buttonSecondary]}
-        >
-          <Image source={giveClassIcon} />
-          <Text style={styles.buttonText}>Dar aulas</Text>
-        </RectButton>
-      </View>
+            <Text style={styles.totalConnections}>
+                Total de {totalConnections} conexões já realizadas {' '}
+                <Image source={heartIcon}/>
+            </Text>
+        </View>
 
-      <Text style={styles.totalConnections}>
-        Total de 200 conexões já realizadas {' '}
-        <Image source={heartIcon} />
-      </Text>
-    </View>
-  )
+
+    )
 }
 
 export default Landing;
